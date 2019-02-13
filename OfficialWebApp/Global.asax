a6059d68124ae4575ef773094abc945836f43dd9
Vanillasushi <Vanillasushi@users.noreply.github.com>
2019-02-13 12:18:18 -0600

    First commit.

---
+++ OfficialWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OfficialWebApp.MvcApplication" Language="C#" %>
---
+++ OfficialWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OfficialWebApp.Startup))]
+namespace OfficialWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
